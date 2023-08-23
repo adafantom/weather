39f85181ad095958e864c85d301c51ebc73eb4e6
adafantom <61115044+adafantom@users.noreply.github.com>
2023-08-23 21:46:18 +0300

    Update main.py
    Добавление структуры ifmain

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import requests
 
 
-city_names = ["Череповец", "Лондон", "SVO"] 
+def main():
+    city_names = ["Череповец", "Лондон", "SVO"] 
+    payload = {
+        "m":"",
+        "M":"",
+        "q":"",
+        "T":"",
+        "n":"",
+        "lang": "ru"
+    }
+    
+    for city in city_names:  
+        url = 'https://wttr.in/{city}'.format(city=city)
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        print(response.text)
 
-payload = {
-    "m":"",
-    "M":"",
-    "q":"",
-    "T":"",
-    "n":"",
-    "lang": "ru"
-}
 
-for city in city_names:  
-    url = 'https://wttr.in/{city}'.format(city=city_names[city])
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
-    print(response.text)
+if __name__ == '__main__':
+    main()
